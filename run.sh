0f639d9196f18df6e199da9141e968ad6e6d951c
bastian-albaut <bastian.albaut@etu.umontpellier.fr>
2023-04-19 11:39:18 +0200

    ✨ feat: Update bash script Run 2 clients despite of 1

--- run.sh
+++ run.sh
@@ -5,17 +5,24 @@ gcc -o client client.c -lpthread
 gcc -o server server.c -lpthread ./utils/list.c
 
 # Open another new terminal window and run server
-if gnome-terminal --title="Server" -- bash -c "./server 5324"; then
+if gnome-terminal --title="Server" -- bash -c "./server 5000"; then
     echo "Server launch successful"
 
 else 
-    echo "Error: Launch server program"
+    echo "Error: Launch Server"
     exit 1
 fi
 
-# Open a new terminal window and run client
-if gnome-terminal --title="Client" -- bash -c "./client 127.0.0.1 5324"; then
-    echo "Client launch successful"
+# Open a new terminal window and run client 1
+if gnome-terminal --title="Client 1" -- bash -c "./client 127.0.0.1 5000"; then
+    echo "Client 1 launch successful"
 else 
-    echo "Error: Launch client pro"
+    echo "Error: Launch Client 1"
+fi
+
+# Open a new terminal window and run client 2
+if gnome-terminal --title="Client 2" -- bash -c "./client 127.0.0.1 5000"; then
+    echo "Client 2 launch successful"
+else 
+    echo "Error: Launch Client 2"
 fi
